7382d01a1307ec2036980a7c708496b8bf587154
Micaela Saenz <70820314+1Nine2@users.noreply.github.com>
2022-10-04 10:37:12 -0400

    Add files via upload

---
+++ Pregunta5.fsx
@@ -0,0 +1,29 @@
+// x, y and return value inferred to be int
+// function1: int -> int -> int
+let function1 x y = x + y
+
+// x, y and return value inferred to be float
+// function2: float -> float -> float
+let function2 (x: float) y = x + y
+// x, y and return value inferred to be int
+// function1: int -> int -> int
+let function3 x y = x - y
+
+// x, y and return value inferred to be float
+// function2: float -> float -> float
+let function4 (x: float) y = x - y
+// x, y and return value inferred to be int
+// function1: int -> int -> int
+let function5 x y = x * y
+
+// x, y and return value inferred to be float
+// function2: float -> float -> float
+let function6 (x: float) y = x * y
+
+// x, y and return value inferred to be int
+// function1: int -> int -> int
+let function7 x y = x / y
+
+// x, y and return value inferred to be float
+// function2: float -> float -> float
+let function8 (x: float) y = x / y
